7cae9374c8354a09ec2cbb2bfea937b6d0cb3d9e
Heti Abhishek Shah <hshah@roshka.com>
2021-10-28 14:05:56 -0300

    New Project

---
+++ curriculumsearch/src/test/java/com/roshka/CurriculumsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roshka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculumsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
