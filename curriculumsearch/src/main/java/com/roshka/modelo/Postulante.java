a33575f0d2b65da72013b5fd70c6cf3e4c8c7b2c
Giuli1297 <giuli1297.gg@gmail.com>
2021-10-28 15:20:22 -0300

    Creacion de clase de Postulante inicial

--- curriculumsearch/src/main/java/com/roshka/modelo/Postulante.java
+++ curriculumsearch/src/main/java/com/roshka/modelo/Postulante.java
@@ -1,5 +1,135 @@
 package com.roshka.modelo;
 
+import java.util.Date;
+
 public class Postulante {
-    
+    private long id;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String ci;
+
+    private String correo;
+
+    private String ciudad;
+
+    private String telefono;
+
+    private Date fechaNacimiento;
+
+    private String resumen;
+
+    private long nivelIngles;
+
+    private String curriculum;
+
+    private String modalidad;
+
+    private String disponibilidad;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCi() {
+        return ci;
+    }
+
+    public void setCi(String ci) {
+        this.ci = ci;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getResumen() {
+        return resumen;
+    }
+
+    public void setResumen(String resumen) {
+        this.resumen = resumen;
+    }
+
+    public long getNivelIngles() {
+        return nivelIngles;
+    }
+
+    public void setNivelIngles(long nivelIngles) {
+        this.nivelIngles = nivelIngles;
+    }
+
+    public String getCurriculum() {
+        return curriculum;
+    }
+
+    public void setCurriculum(String curriculum) {
+        this.curriculum = curriculum;
+    }
+
+    public String getModalidad() {
+        return modalidad;
+    }
+
+    public void setModalidad(String modalidad) {
+        this.modalidad = modalidad;
+    }
+
+    public String getDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(String disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
 }
