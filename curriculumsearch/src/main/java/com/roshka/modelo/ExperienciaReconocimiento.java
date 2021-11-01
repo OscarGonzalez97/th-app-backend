6c7fe450b2e87720ba985d5d401a6d8321f82a27
Joel <jflor999@gmail.com>
2021-11-01 17:31:12 -0300

    reconocimiento exp corregido modelo

--- curriculumsearch/src/main/java/com/roshka/modelo/ExperienciaReconocimiento.java
+++ curriculumsearch/src/main/java/com/roshka/modelo/ExperienciaReconocimiento.java
@@ -14,8 +14,11 @@ public class ExperienciaReconocimiento {
     @JoinColumn
     private Experiencia experiencia;
   
-    @Column(name = "nivel")
-    private long nivel;
+    @Column(name="nombre")
+    private String nombre;
+
+    @Column(name="certificado")
+    private String certificado;
 
     public long getId() {
         return id;
@@ -29,10 +32,16 @@ public class ExperienciaReconocimiento {
     public void setExperiencia(Experiencia experiencia) {
         this.experiencia = experiencia;
     }
-    public long getNivel() {
-        return nivel;
+    public String getCertificado() {
+        return certificado;
     }
-    public void setNivel(long nivel) {
-        this.nivel = nivel;
+    public String getNombre() {
+        return nombre;
+    }
+    public void setCertificado(String certificado) {
+        this.certificado = certificado;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 }
