4b088c467ece23bac5be764a9173bd65f973db7e
Javier Ferreira <jafere98@gmail.com>
2021-11-04 08:47:02 -0300

    filtro personas con experencia mayor a un año

--- curriculumsearch/src/main/java/com/roshka/repositorio/PostulanteRepository.java
+++ curriculumsearch/src/main/java/com/roshka/repositorio/PostulanteRepository.java
@@ -11,4 +11,11 @@ public interface PostulanteRepository extends JpaRepository<Postulante,Long> {
 
     @Query("select p from Postulante p join p.estudios e on e.institucion LIKE %?1%")
 	public List<Postulante> findByInstitucionEstudio(String institucion);
+    @Query(value = "select p" +
+        "from postulante p join experiencia x on x.postulante_id= p.id" +
+        "group by p.id" +
+        "having sum(fecha_hasta-fecha_desde)>?1 * INTERVAL '1' month",
+        nativeQuery = true )
+    public List<Postulante> personasConExperienciaMayor(long meses);        
+
 }
