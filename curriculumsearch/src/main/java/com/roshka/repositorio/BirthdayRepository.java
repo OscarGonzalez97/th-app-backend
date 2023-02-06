53166f92efa7de0a12a09e39b1e72e069d2bebf1
Oscar Gonzalez <oscargonzalez159@gmail.com>
2023-02-06 11:39:23 -0300

    se agrega metodo para filtrar por fecha

--- curriculumsearch/src/main/java/com/roshka/repositorio/BirthdayRepository.java
+++ curriculumsearch/src/main/java/com/roshka/repositorio/BirthdayRepository.java
@@ -6,7 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Date;
+import java.util.List;
+
 public interface BirthdayRepository extends JpaRepository<Birthday,Long> {
+
+    List<Birthday> findAllByFecha(Date publicationDate);
 
     public Page<Birthday> findByNombreCompletoContainingIgnoreCase(String nombre, Pageable pageable);
 
